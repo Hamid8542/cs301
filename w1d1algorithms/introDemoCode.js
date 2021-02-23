bc47a14c48b43bca571e6f5275a9662330d154d1
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-22 23:17:18 -0600

    w1d1assignment - first commit

---
+++ w1d1algorithms/introDemoCode.js
@@ -0,0 +1,23 @@
+// This is Abdulhamid Ibrahim Student @MIU 
+// <><><><> This is my first javascript DemoCode
+
+let message = " Welcome to Intro to programing with JavaScript ! ";
+console.log(message)
+
+let courseCode = "  The course code is CS 301 ";
+console.log(courseCode)
+
+let instructorName = " Keith Levi, Ph.D "
+console.log(instructorName);
+
+// let's do demo code
+
+let x = 20;
+let y = 30;
+let sum = x + y;
+
+console.log(sum);
+
+const myBirthday = "10/10/2010";
+
+console.log(myBirthday)
